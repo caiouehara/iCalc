5c4afddee0e00dcd5d6b5f5ca9c8c97add485b14
caiouehara <caioueharamartins@gmail.com>
2023-06-18 23:20:51 -0300

    Criado leitura de arquivo TXT

--- calculadoraMMQ/source/main.c
+++ calculadoraMMQ/source/main.c
@@ -4,21 +4,23 @@
 
 typedef float (*callbacks[])(float x);
 
-float g1(float x){
-    return (0.4*x);
+float g1(float x)
+{
+    return (0.4 * x);
 }
 
-float g2(float x){
-    return (1*x);
+float g2(float x)
+{
+    return (1 * x);
 }
 
 callbacks funcoesParametros =
-{
-  g1,
-  g2
-};
+    {
+        g1,
+        g2};
 
-void sistemaNormal(float* fx, float* x, callbacks g, int n){
+void sistemaNormal(float *fx, float *x, callbacks g, int n)
+{
     float a1 = 0;
     float a2 = 0;
     float numA1 = 0;
@@ -32,8 +34,9 @@ void sistemaNormal(float* fx, float* x, callbacks g, int n){
     float g1_fx = 0;
     float g2_fx = 0;
 
-    //Elementos do sistema normal (discreto)
-    for(int i=0; i < n; i++){
+    // Elementos do sistema normal (discreto)
+    for (int i = 0; i < n; i++)
+    {
         g1_g1 = g[0](x[i]) * g[0](x[i]);
         g1_g2 = g[0](x[i]) * g[1](x[i]);
         g2_g1 = g[1](x[i]) * g[0](x[i]);
@@ -42,17 +45,42 @@ void sistemaNormal(float* fx, float* x, callbacks g, int n){
         g2_fx = g[1](x[i]) * fx[i];
     }
 
-    //Crammer Rule,
+    // Crammer Rule,
     numA1 = (g1_fx * g2_g2) - (g1_g2 * g2_fx);
     numA2 = (g1_g1 * g2_fx) - (g1_fx * g2_g1);
     denom = (g1_g1 * g2_g2) - (g1_g2 * g2_g1);
 
-    a1 = numA1/denom;
-    a2 = numA2/denom;
+    a1 = numA1 / denom;
+    a2 = numA2 / denom;
 
     printf("a: %d, b: %d", a1, a2);
 };
 
-int main(){
+int main()
+{
+    char path[] = "../dadosEx2.txt";
 
+    FILE *stream;
+    stream = fopen(path, "r");
+    if (NULL == stream)
+    {
+        printf("Arquivo nao pode ser aberto\n");
+        return -1;
+    }
+    
+    int n = 101;
+    int i[n];
+    float t_i[n];
+    float y_i[n];
+
+    int MAX_CARACTERES = 50;
+    char linha[MAX_CARACTERES];
+    while (fgets(linha, MAX_CARACTERES, stream) && !feof(stream))
+    {
+        printf(linha);
+    }
+    fclose(stream);
+    printf("File read\n");
+
+    return 0;
 }
